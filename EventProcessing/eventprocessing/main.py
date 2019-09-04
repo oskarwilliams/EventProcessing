f47e0658cda47920665610405152e1daf99e007e
Oskar <oskar.williams@softwire.com>
2019-09-04 17:34:40 +0100

    Refactoring minorly

--- EventProcessing/eventprocessing/main.py
+++ EventProcessing/eventprocessing/main.py
@@ -1,6 +1,7 @@
 import boto3
 import ast
 import json
+import time
 
 
 def allow_sns_to_write_to_sqs(topicarn, queuearn):
@@ -55,9 +56,63 @@ def startup(topicArn, queueName):
     return queue
 
 
-def printMessage(queue):
+def writeAverageValue(averageCounter, end, messageArrays):
+    totalValue = 0
+    numOfMessages = 0
+
+    for messageArray in messageArrays:
+        for message in messageArray:
+            if message['timestamp'] > (end - 360)*1000 and message['timestamp'] < (end - 300)*1000:
+                totalValue += message['value']
+                numOfMessages += 1
+
+    if numOfMessages != 0:
+        averageValue = totalValue/numOfMessages
+
+        with open("outputAverageValue.json", "a") as outfile:
+            if averageCounter != 0:
+                outfile.write(', ')
+            json.dump({
+                'averageValue': averageValue,
+                'startTime': end - 360,
+                'endTime': end - 300
+            }, outfile)
+        averageCounter += 1
+    return averageCounter
+
+
+def writeRawOutput(messageDict, messageArrays, locationsDict, rawCounter):
+
+    if len(list(filter(lambda location: location['id'] == messageDict['locationId'], locationsDict))) != 0:
+        doesntAlreadyExists = True
+        for messageArray in messageArrays:
+            if messageDict in messageArray:
+                doesntAlreadyExists = False
+
+        if doesntAlreadyExists:
+            with open("outputRaw.json", "a") as outfile:
+                if rawCounter != 0:
+                    outfile.write(', ')
+                json.dump(messageDict, outfile)
+            rawCounter += 1
+            messageArrays[0] += [messageDict]
+    return messageArrays, rawCounter
+
+
+def logMessages(queue):
     with open('locations.json', 'r') as f:
-        locations_dict = json.load(f)
+        locationsDict = json.load(f)
+    with open("outputRaw.json", "w") as outfile:
+        outfile.write('[')
+    with open("outputAverageValue.json", "w") as outfile:
+        outfile.write('[')
+
+    start = time.time()
+    rawCounter = 0
+    averageCounter = 0
+    messageArrays = [[]]
+    messageArrays[0] = []
+
     try:
         while True:
             messages = queue.receive_messages(AttributeNames=[
@@ -66,23 +121,39 @@ def printMessage(queue):
                 MaxNumberOfMessages=10,
                 WaitTimeSeconds=10,
             )
+
             for message in messages:
                 messageBodyDict = ast.literal_eval(message.body)
                 messageDict = ast.literal_eval(messageBodyDict['Message'])
-                location = messageDict['locationId']
-                if len(list(filter(lambda validLocation: validLocation['id'] == location, locations_dict))) != 0:
-                    print(messageDict)
+                messageArrays, rawCounter = writeRawOutput(
+                    messageDict, messageArrays, locationsDict, rawCounter)
+
+            end = time.time()
+
+            if end - start > 60:
+
+                averageCounter = writeAverageValue(
+                        averageCounter, end, messageArrays)   
+                start = time.time()
+
+                for i in range(9, 0):
+                    if i == 0:
+                        messageArrays[i] = []
+                    else:
+                        messageArrays[i] = messageArrays[i-1]
 
     except KeyboardInterrupt:
         print('Shutting Down Gracefully')
+        with open("outputRaw.json", "a") as outfile:
+            outfile.write(']')
+        with open("outputAverageValue.json", "a") as outfile:
+            outfile.write(']')
+
         boto3.client('sqs').delete_queue(
             QueueUrl=queue.url
         )
 
-
-
-
 topicArn = 'arn:aws:sns:eu-west-1:552908040772:EventProcessing-Stack1-snsTopicSensorDataPart1-MEC8N0UVX4AR'
 queueName = 'locationsQueueOW'
 queue = startup(topicArn, queueName)
-printMessage(queue)
+logMessages(queue)
