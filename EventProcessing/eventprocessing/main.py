362d25fa1833c6907ec0382b5eb662d3a18866e9
Oskar <oskar.williams@softwire.com>
2019-09-05 12:27:46 +0100

    Part 2 complete, part 3 begun

--- EventProcessing/eventprocessing/main.py
+++ EventProcessing/eventprocessing/main.py
@@ -2,7 +2,7 @@ import boto3
 import ast
 import json
 import time
-
+import logging
 
 def allow_sns_to_write_to_sqs(topicarn, queuearn):
     policy_document = """{{
@@ -26,37 +26,49 @@ def allow_sns_to_write_to_sqs(topicarn, queuearn):
     return policy_document
 
 
-def startup(topicArn, queueName):
-
+def connectToBucket(bucketName):
     s3 = boto3.resource('s3')
-    sqs = boto3.resource('sqs')
-    sns = boto3.resource('sns')
-    topic = sns.Topic(topicArn)
-    bucket = s3.Bucket('eventprocessing-stack1-locationss3bucket-ivyo76ekdc6y')
+    logging.info(f'Connecting to S3 Bucket: {bucketName}')
+    bucket = s3.Bucket(bucketName)
+    logging.info('Downloading locations.json')
+    bucket.download_file('locations.json', 'locations.json')
+    return bucket
 
+def setupQueue(queueName, topicArn):
+    sqs = boto3.resource('sqs')
+    logging.info(f'Creating queue: {queueName}')
     queue = sqs.create_queue(QueueName=queueName)
     queueArn = queue.attributes.get('QueueArn')
-
     policy_json = allow_sns_to_write_to_sqs(topicArn, queueArn)
-
     response = boto3.client('sqs').set_queue_attributes(
         QueueUrl=queue.url,
         Attributes={
             'Policy': policy_json
         }
     )
+    return queue, queueArn
 
+
+def subscribeQueue(topicArn, queueArn):
+    sns = boto3.resource('sns')
+    logging.info('Subscribing queue to topic: ' + topicArn)
+    topic = sns.Topic(topicArn)
     notifier = topic.subscribe(
         Protocol='sqs',
         Endpoint=queueArn,
         ReturnSubscriptionArn=True
     )
 
-    bucket.download_file('locations.json', 'locations.json')
+
+def startup(topicArn, queueName, bucketName):
+
+    connectToBucket(bucketName)
+    queue, queueArn = setupQueue(queueName, topicArn)
+    subscribeQueue(topicArn, queueArn)
     return queue
 
 
-def writeAverageValue(averageCounter, end, messageArrays):
+def writeAverageValue(averageCounter, end, messageArrays, outputName):
     totalValue = 0
     numOfMessages = 0
 
@@ -69,7 +81,7 @@ def writeAverageValue(averageCounter, end, messageArrays):
     if numOfMessages != 0:
         averageValue = totalValue/numOfMessages
 
-        with open("outputAverageValue.json", "a") as outfile:
+        with open(f"outputs/{outputName}", "a") as outfile:
             if averageCounter != 0:
                 outfile.write(', ')
             json.dump({
@@ -81,7 +93,7 @@ def writeAverageValue(averageCounter, end, messageArrays):
     return averageCounter
 
 
-def writeRawOutput(messageDict, messageArrays, locationsDict, rawCounter):
+def writeRawOutput(messageDict, messageArrays, locationsDict, rawCounter, outputName):
 
     if len(list(filter(lambda location: location['id'] == messageDict['locationId'], locationsDict))) != 0:
         doesntAlreadyExists = True
@@ -90,31 +102,34 @@ def writeRawOutput(messageDict, messageArrays, locationsDict, rawCounter):
                 doesntAlreadyExists = False
 
         if doesntAlreadyExists:
-            with open("outputRaw.json", "a") as outfile:
-                if rawCounter != 0:
-                    outfile.write(', ')
-                json.dump(messageDict, outfile)
+            # with open(f"outputs/{outputName}", "a") as outfile:
+            #     if rawCounter != 0:
+            #         outfile.write(', ')
+            #     json.dump(messageDict, outfile)
             rawCounter += 1
             messageArrays[0] += [messageDict]
     return messageArrays, rawCounter
 
 
-def logMessages(queue):
-    with open('locations.json', 'r') as f:
-        locationsDict = json.load(f)
-    with open("outputRaw.json", "w") as outfile:
-        outfile.write('[')
-    with open("outputAverageValue.json", "w") as outfile:
+def logMessages(queue, outputRawName, outputAverageName):
+    with open('locations.json', 'r') as infile:
+        locationsDict = json.load(infile)
+    # with open(f"outputs/{outputRawName}", "w") as outfile:
+        # outfile.write('[')
+    with open(f"outputs/{outputAverageName}", "w") as outfile:
         outfile.write('[')
 
-    start = time.time()
+    startTime = time.time()
     rawCounter = 0
     averageCounter = 0
     messageArrays = [[]]
     messageArrays[0] = []
 
     try:
+        logging.info('Taking messages from queue')
+        logging.info(f'Start time is {startTime}')
         while True:
+
             messages = queue.receive_messages(AttributeNames=[
                 'All'
             ],
@@ -126,15 +141,16 @@ def logMessages(queue):
                 messageBodyDict = ast.literal_eval(message.body)
                 messageDict = ast.literal_eval(messageBodyDict['Message'])
                 messageArrays, rawCounter = writeRawOutput(
-                    messageDict, messageArrays, locationsDict, rawCounter)
+                    messageDict, messageArrays, locationsDict, rawCounter, outputRawName)
+                message.delete()
 
-            end = time.time()
+            endTime = time.time()
 
-            if end - start > 60:
+            if endTime - startTime > 60:
 
                 averageCounter = writeAverageValue(
-                        averageCounter, end, messageArrays)   
-                start = time.time()
+                    averageCounter, endTime, messageArrays, outputAverageName)
+                startTime = time.time()
 
                 for i in range(9, 0):
                     if i == 0:
@@ -144,16 +160,19 @@ def logMessages(queue):
 
     except KeyboardInterrupt:
         print('Shutting Down Gracefully')
-        with open("outputRaw.json", "a") as outfile:
-            outfile.write(']')
-        with open("outputAverageValue.json", "a") as outfile:
+        # with open(f"outputs/{outputRawName}", "a") as outfile:
+        #     outfile.write(']')
+        with open(f"outputs/{outputAverageName}", "a") as outfile:
             outfile.write(']')
 
-        boto3.client('sqs').delete_queue(
-            QueueUrl=queue.url
-        )
+        queue.delete()
 
-topicArn = 'arn:aws:sns:eu-west-1:552908040772:EventProcessing-Stack1-snsTopicSensorDataPart1-MEC8N0UVX4AR'
-queueName = 'locationsQueueOW'
-queue = startup(topicArn, queueName)
-logMessages(queue)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    topicArn = 'arn:aws:sns:eu-west-1:552908040772:EventProcessing-Stack1-snsTopicSensorDataPart1-MEC8N0UVX4AR'
+    queueName = 'locationsQueueOW'
+    bucketName = 'eventprocessing-stack1-locationss3bucket-ivyo76ekdc6y'
+    outputRawName = 'outputRaw.json'
+    outputAverageName = 'outputAverageValue.json'
+    queue = startup(topicArn, queueName, bucketName)
+    logMessages(queue, outputRawName, outputAverageName)
--- EventProcessing/tests/test_eventprocessing.py
+++ EventProcessing/tests/test_eventprocessing.py
@@ -1,5 +1,18 @@
 from eventprocessing import __version__
+from eventprocessing.main import connectToBucket, setupQueue
+import pytest
 
+topicArn = 'arn:aws:sns:eu-west-1:552908040772:EventProcessing-Stack1-snsTopicSensorDataPart1-MEC8N0UVX4AR'
+queueName = 'locationsQueueOW'
+bucketName = 'eventprocessing-stack1-locationss3bucket-ivyo76ekdc6y'
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_bucketConnecting():
+    assert connectToBucket(bucketName).name == bucketName
+
+def test_setupQueue():
+    queue, queueArn = setupQueue(queueName, topicArn)
+    assert queueArn == 'arn:aws:sqs:eu-west-1:552908040772:locationsQueueOW'
+    queue.delete()
